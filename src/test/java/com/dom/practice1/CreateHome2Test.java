a2d7f119b725836c53e486f036fde5032b5d9579
HAPPY <HAPPY@LAPTOP-LIP92R15>
2022-03-24 22:10:41 +0530

    first commit

---
+++ src/test/java/com/dom/practice1/CreateHome2Test.java
@@ -0,0 +1,11 @@
+package com.dom.practice1;
+
+import org.testng.annotations.Test;
+
+public class CreateHome2Test {
+	@Test
+	public void CrateHome2() {
+		System.out.println("user1 home2");
+	}
+
+}
---
+++ src/test/java/com/dom/practice1/CreateHomeTest.java
@@ -0,0 +1,11 @@
+package com.dom.practice1;
+
+import org.testng.annotations.Test;
+
+public class CreateHomeTest {
+	@Test
+	public void CreateHome() {
+		System.out.println("user1 home");
+	}
+
+}
